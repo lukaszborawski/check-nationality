91f90897e66786cf4a22746f24d48cc44c75007a
Łukasz Borawski <lukasz.borawski@gmail.com>
2022-09-15 13:42:02 +0200

    fixed validation

--- main.js
+++ main.js
@@ -1,51 +1,66 @@
-const nameInput = document.querySelector(".name-input")
-const resultInput = document.querySelector(".result-input")
-const submitButton = document.querySelector(".submit-btn")
-const copyButton = document.querySelector(".copy-btn")
+const nameInput = document.querySelector(".name-input");
+const resultInput = document.querySelector(".result-input");
+const submitButton = document.querySelector(".submit-btn");
+const copyButton = document.querySelector(".copy-btn");
+let errorText = document.querySelector(".error-text");
 
 let arrayOfName;
 
 let getData = async (e) => {
-  e.preventDefault();
   let inputValue = nameInput.value.toLowerCase().trim();
-  const reg = /[A-Za-z]/
-  if (!reg.test(inputValue)) {
+  resultInput.value = "";
+  if (!nameInput.checkValidity()) {
+    errorText.innerHTML = "";
     return;
   } else {
+    e.preventDefault();
     if (localStorage.getItem("names") === null) {
       arrayOfName = [];
     } else {
       arrayOfName = JSON.parse(localStorage.getItem("names"));
     }
     if (arrayOfName.find(el => el === inputValue)) {
+      errorText.innerHTML = "<h3>The name has already been used</h3>";
       resultInput.value = "";
       return;
     } else {
       try {
-        let inputValue = nameInput.value.toLowerCase().trim()
-        arrayOfName.push(inputValue)
+        errorText.innerHTML = "";
+        let inputValue = nameInput.value.toLowerCase().trim();
+        arrayOfName.push(inputValue);
         localStorage.setItem("names", JSON.stringify(arrayOfName));
-        console.log(arrayOfName)
-        let response = await fetch(`https://api.genderize.io?name=${inputValue}`)
+        let response = await fetch(`https://api.genderize.io?name=${inputValue}`);
         if (!response.ok) {
           throw new Error(`Http error: ${response.statusText}`);
         }
-        let data = await response.json()
-        resultInput.value = `Gender: ${data.gender}`;
+        let data = await response.json();
+        if (data.gender === null || data.gender === undefined) {
+          errorText.innerHTML = "<h3>Error. Check the name and try again.</h3>";
+          resultInput.value = "";
+        }
+        else {
+          resultInput.value = `Gender: ${data.gender}`;
+        }
       } catch (error) {
-        console.log(`${error}`)
+        console.log(`${error}`);
       }
     }
     try {
-      let inputValue = nameInput.value.toLowerCase().trim()
-      let response = await fetch(`https://api.nationalize.io/?name=${inputValue}`)
+      let inputValue = nameInput.value.toLowerCase().trim();
+      let response = await fetch(`https://api.nationalize.io/?name=${inputValue}`);
       if (!response.ok) {
         throw new Error(`Http error: ${response.statusText}`);
       }
-      let data = await response.json()
-      resultInput.value = `${resultInput.value}, Nationality: ${data.country[0].country_id}`;
+      let data = await response.json();
+      if (data.country[0].country_id === null || data.country[0].country_id === undefined) {
+        errorText.innerHTML = "<h3>Error. Check the name and try again.</h3>";
+        resultInput.value = "";
+      }
+      else {
+        resultInput.value = `${resultInput.value}, Nationality: ${data.country[0].country_id}`;
+      }
     } catch (error) {
-      console.log(`${error}`)
+      console.log(`${error}`);
     }
   }
 }
@@ -56,5 +71,5 @@ let getCopy = (e) => {
   document.execCommand("copy");
 }
 
-submitButton.addEventListener("click", getData)
-copyButton.addEventListener("click", getCopy)
+submitButton.addEventListener("click", getData);
+copyButton.addEventListener("click", getCopy);
